61e4bb547f91bdd671c646b803bc93ad738db9b0
Petryck WIlliam <petryck@i9devgroup.com>
2022-05-04 16:26:45 -0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const conexao = require('../server/conexao/connect.js');
+const conexao_sql = require('../server/conexao/connect_sql.js');
+const rotas = require('../server/rotas.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app);
+app.use(require("cors")());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(rotas)
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+
+conexao_sql.conecta_sql();
+conexao.incia_conexao();
+
+
+server.listen(7070, function () {
+    console.log(`Servidors Carregado ${server.address().port}`);
+});
---
+++ server/rotas.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const formidable = require('formidable');
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES;  
+const router = express.Router();
+const mysql = require('mysql');
+var nodemailer = require('nodemailer');
+const request = require("request-promise");
+
+ 
+const appRoot = require('app-root-path');
+router.use(express.static(appRoot + '/public'));
+http = require('http')
+
+
+
+
+router.get('/', function (req, res) {
+  
+  if(req.query.login == 'PowerTrade' && req.query.senha == 'minhapower@2022ç'){
+
+    referencia = req.query.referencia;
+
+
+    sql = `SELECT TOP 10 * FROM vis_Tracking_Portal_Follow_API WHERE Numero_Processo = '${referencia}' OR Referencia_Cliente = '${referencia}' ORDER BY Data ASC`;
+    trans = {};
+CONEXA_HEAD.execSql(new Request(sql, function(err, rowCount, rows){
+  if(err) {
+      throw err;
+  }
+}).on('doneInProc',function(rowCount_transbodo, more2, rows_transbodo){
+var contun2 = 0;
+
+rows_transbodo.forEach(function (column_trans) {
+  trans[contun2] = {};
+  
+  column_trans.forEach(function (column_trans_entro) {
+  
+    if(column_trans_entro.value == null){
+      column_trans_entro.value = '';
+    }
+    
+
+ 
+    trans[contun2][column_trans_entro.metadata.colName] = column_trans_entro.value;
+
+
+
+  });
+
+
+  contun2 = contun2 + 1;
+})
+
+
+}).on('requestCompleted',function(rowCount, more, rows){
+
+
+res.json(trans);
+
+
+
+}));
+
+  }else{
+    res.json('acesso negado')
+  }
+  
+  
+});
+
+
+
+
+
+module.exports = router;
